10feb122b93965376d0dd81e830379dc1bef9670
ikaipu <kabikabitznz@gmail.com>
2017-12-31 15:45:51 +0800

    refactor

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,51 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using UnityEngine;
+using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
 namespace Script
 {
 	public class GameManager {
-		private List<IPlayer> _players;
+		private List<IPlayer> _players { get; set; }
+		private List<EnumRole> _roles { get; set; }
 
-		public void InitPlayers (List<IPlayer> players)
+		public GameManager(List<IPlayer> players, List<EnumRole> roles)
 		{
 			_players = players;
-			Debug.Log ("Players:");
-			_players.ForEach (player => Debug.Log(player.id));
-		}
-
-		public void AssignRoles(List<EnumRole> roles) {
-			var shuffledRoles = roles.Shuffle();
-			for (var i = 0; i < _players.Count; i++) {
-				_players [i].role = shuffledRoles[i];
-			}
-			Debug.Log ("Assigned Roles:");
-			_players.ForEach (player => Debug.Log(player.id + ":" + player.role));
-		}
-
-		public void StartVotingPhase(){
-			for (var i = 0; i < _players.Count; i++)
-			{
-				Func<string> RandomSelectPlayer = () =>
-				{
-					var candidates = _players.FindAll(player => player.id != _players[i].id);
-					return candidates [Random.Range (0, candidates.Count)].id;
-				};
-				_players[i].Vote(_players, RandomSelectPlayer);
-			}
-			Debug.Log ("Vote Result:");
-			_players.ForEach (player => Debug.Log(player.id + ":" + player.votedNum));
+			_roles = roles;
 		}
 		
-		public void ShowResult() {
+		public void PrepareGame() {
+			InitPlayers(_players);
+			AssignRoles(_roles);
+		}
+		
+		public System.Collections.IEnumerator PlayGame()
+		{
+			ProcessDayPhase();
+			yield return ProcessVotingPhase();
+			ShowResult();
+		}
+
+		private void ProcessDayPhase()
+		{
+			
+		}
+		
+		private void ShowResult() {
 			var maxVotedNum = _players.ConvertAll (player => player.votedNum).Max ();
 			var executedPlayers = _players.FindAll (player => player.votedNum == maxVotedNum);
 			Debug.Log ("ExecutedPlayers:");
 			executedPlayers.ForEach (player => Debug.Log(player.id + ":" + player.role));
 			var isCitiznTeamWinner = executedPlayers.Exists (player => player.role == EnumRole.Werewolf );
 			Debug.Log(isCitiznTeamWinner ? "Citizen team win!!" : "Werewolf team win!!");
+		}
+
+		private System.Collections.IEnumerator ProcessVotingPhase()
+		{
+			foreach (IPlayer _player in _players)
+			{
+				Func<string> RandomSelectPlayer = () =>
+				{
+					var candidates = _players.FindAll(player => player.id != _player.id);
+					return candidates [Random.Range (0, candidates.Count)].id;
+				};
+				_player.Vote(_players, RandomSelectPlayer);
+			}
+			Debug.Log ("Vote Result:");
+			_players.ForEach (player => Debug.Log(player.id + ":" + player.votedNum));
+			yield return null;
+		}
+
+		private void InitPlayers (List<IPlayer> players)
+		{
+			_players = players;
+			Debug.Log ("Players:");
+			_players.ForEach (player => Debug.Log(player.id));
+		}
+
+		private void AssignRoles(List<EnumRole> roles) {
+			var shuffledRoles = roles.Shuffle();
+			for (var i = 0; i < _players.Count; i++) {
+				_players [i].role = shuffledRoles[i];
+			}
+			Debug.Log ("Assigned Roles:");
+			_players.ForEach (player => Debug.Log(player.id + ":" + player.role));
 		}
 	}
 }
--- Assets/Script/WerewolfController.cs
+++ Assets/Script/WerewolfController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -7,48 +8,50 @@ using UnityEngine.UI;
 
 namespace Script
 {
-	public class WerewolfController : MonoBehaviour
-	{
+    public class WerewolfController : MonoBehaviour
+    {
+        public List<Button> ButtonList;
 
-		public List<Button> ButtonList;
+        // Use this for initialization
+        private void Start()
+        {
+            StartCoroutine(PlayGame());
+        }
 
-		// Use this for initialization
-		private void Start () {
-			ButtonList.ForEach(Button =>
-			{
-				Button.enabled = false;
-			});
-			Debug.Log ("Start:");
-			var gameManager = new GameManager ();
-			var playerIds = new List<string> {
-				"Takumi",
-				"John",
-				"Gong",
-				"Ketchup"
-			};
-			AssignPlayerName(playerIds);
-			var players = playerIds.ConvertAll(id => (IPlayer) new Player(id));
-			
-			gameManager.InitPlayers (players);
-			var roles = new List<EnumRole> {
-				EnumRole.Citizen,
-				EnumRole.Citizen,
-				EnumRole.Citizen,
-				EnumRole.Werewolf
-			};
-			
-			gameManager.AssignRoles (roles);
-			gameManager.StartVotingPhase ();
-			gameManager.ShowResult ();
-		}
-		
-		private void AssignPlayerName(List<string> playerIds)
-		{
-			for (int i = 0; i < playerIds.Count; i++)
-			{
-				ButtonList[i].GetComponentInChildren<Text>().text = playerIds[i];
-			}
-			//
-		}
-	}
+        private IEnumerator PlayGame()
+        {
+            ButtonList.ForEach(Button => { Button.enabled = false; });
+            Debug.Log("Start:");
+            var playerIds = new List<string>
+            {
+                "Takumi",
+                "John",
+                "Gong",
+                "Ketchup"
+            };
+            AssignPlayerName(playerIds);
+            var players = playerIds.ConvertAll(id => (IPlayer) new Player(id));
+            var roles = new List<EnumRole>
+            {
+                EnumRole.Citizen,
+                EnumRole.Citizen,
+                EnumRole.Citizen,
+                EnumRole.Werewolf
+            };
+            
+            var gameManager = new GameManager(players, roles);
+
+            gameManager.PrepareGame();
+            yield return gameManager.PlayGame();
+        }
+
+        private void AssignPlayerName(List<string> playerIds)
+        {
+            for (int i = 0; i < playerIds.Count; i++)
+            {
+                ButtonList[i].GetComponentInChildren<Text>().text = playerIds[i];
+            }
+            //
+        }
+    }
 }
